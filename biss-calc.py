aa2c792e48587c7cb49b85d203841e9d9c105386
Kevin Peterson <kmpeters@anl.gov>
2026-08-18 11:57:14 -0500

    Require the encoder's resolution to be specified. Changed the -b argument to -r to allow a position to be reported using a reduced number of position bits

--- biss-calc.py
+++ biss-calc.py
@@ -33,7 +33,7 @@ def crcValidation(data, crcToValidate):
 	return (crc == crcToValidate)
 
 
-def parseBiSSdata(data, bits):
+def parseBiSSdata(data, encBits, reducedBits):
 	if len(data) != 64:
 		print("Error: data length (%d) isn't 64".format(len(data)))
 		return -1
@@ -52,9 +52,9 @@ def parseBiSSdata(data, bits):
 	#!print("bits {}", bits)
 	
 	# Extract important data
-	posData = data[idx:idx+bits]
-	crcData = data[idx:idx+bits+2]
-	idx += bits
+	posData = data[idx:idx+encBits]
+	crcData = data[idx:idx+encBits+2]
+	idx += encBits
 	encErr = int(data[idx])
 	idx += 1
 	encWarn = int(data[idx])
@@ -65,27 +65,30 @@ def parseBiSSdata(data, bits):
 	#!print("010", posData, encErr, encWarn, crc, ignored)
 	
 	# 
-	print("Encoder status (1=OK): Error = {}, Warning = {}".format(encErr, encWarn))
+	print("Encoder status: Error = {}, Warning = {}  (1=OK)".format(encErr, encWarn))
 	
 	# Validate CRC
+	#!print(crc, crcData)
 	crcResult = crcValidation(int(crcData, base=2), int(crc, base=2))
 	#!print(crcResult)
 	
 	if crcResult:
 		print("CRC check OK")
-		
-		# Convert position
-		#!print(posData)
-		posDataint = int(posData, base=2)
-		
-		print("{}-bit position: {}".format(bits, posDataint))
-		#print("{}-bit position: {}".format(bits, (posDataint >> (32 - bits))))
 	else:
 		print("CRC check FAILED")
+		
+	# Convert position even if the CRC check failed
+	#!print(posData)
+	posDataint = int(posData, base=2)
+	
+	print("{}-bit position: {}".format(encBits, posDataint))
+	if reducedBits != 0:
+		print("{}-bit position: {}".format(reducedBits, (posDataint >> (encBits - reducedBits))))
 
 
 def main(args):
-	bits = args.bits
+	encBits = args.enc_bits
+	reducedBits = args.reduced_bits
 	hexStr = args.hex_data
 	# c0a1685e9f3bcc0a
 	#!print(hexStr)
@@ -96,7 +99,7 @@ def main(args):
 	# 1100000010100001011010000101111010011111001110111100110000001010
 	print(binStr)
 	
-	parseBiSSdata(binStr, bits)
+	parseBiSSdata(binStr, encBits, reducedBits)
 
 
 if __name__ == '__main__':
@@ -106,8 +109,9 @@ if __name__ == '__main__':
 	
 	parser = ap.ArgumentParser("biss-calc.py")
 	
+	parser.add_argument("enc_bits", action="store", type=int, default=None, help="BiSS-C encoder resolution")
 	parser.add_argument("hex_data", action="store", default=None, help="BiSS-C hex data")
-	parser.add_argument("-b", action="store", dest="bits", type=int, default=0, help="BiSS-C data bits")
+	parser.add_argument("-r", action="store", dest="reduced_bits", type=int, default=0, help="Reduced data bits")
 	
 	args = parser.parse_args(sys.argv[1:])
 	
